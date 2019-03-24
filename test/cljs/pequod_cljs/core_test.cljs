ea248039c6d0f1ff639c2505bcfd82a8d2019b6f
Mitchell Szczepanczyk <mitchell@szcz.org>
2019-03-23 19:43:22 -0500

    Yet more tests!

--- test/cljs/pequod_cljs/core_test.cljs
+++ test/cljs/pequod_cljs/core_test.cljs
@@ -61,6 +61,122 @@
                                 :init-final-price 100
                                 :init-labor-price 150}))))
 
+
+(deftest proposal-and-solutions
+  (let [wc-3 {:effort 0.5, :cq 0.25, :ce 1, :a 0.25, :labor-exponents '(0.3344715806333696), :industry 1, :output 0, :du 7, :c 0.05, :product 2, :labor-quantities [0], :production-inputs ['(3) '(1) '(1)], :input-exponents '(0.35704369449641105), :xe 0.05, :s 1, :nature-exponents '(0.40775789400671814)}
+        wc-4 {:effort 0.5, :cq 10, :ce 1, :a 10, :labor-exponents [0.2327136009634174], :industry 0, :output 0, :du 2, :c 0.05, :product 2, :labor-quantities [0], :production-inputs [[1 2] [1] [1]], :input-exponents [0.18730943766373953 0.1700289432131638], :xe 0.05, :s 1, :nature-exponents [0.1970497669613798]}
+        wc-5 {:effort 0.5, :cq 10, :ce 1, :a 10, :labor-exponents [0.24925680571914088], :industry 0, :output 0, :du 2, :c 0.05, :product 1, :labor-quantities [0], :production-inputs [[1 2 3] [1] [1]], :input-exponents [0.07540314861302111 0.10098780164832724 0.10718250832816038], :xe 0.05, :s 1, :nature-exponents [0.17057223193493595]}
+        wc-6 {:effort 0.5, :cq 10, :ce 1, :a 10, :labor-exponents [0.15813469104723293], :industry 0, :output 0, :du 2, :c 0.05, :product 2, :labor-quantities [0], :production-inputs [[1 2 3 4] [1] [1]], :input-exponents [0.07136063178539023 0.09705394605246723 0.09837585447317158 0.0516097801370993], :xe 0.05, :s 1, :nature-exponents [0.1978580219892075]}
+        input-prices [100 100 100 100]
+        nature-prices [150]
+        labor-prices [150]
+ ]
+    (is (= {:effort 40.31122251385122,
+            :cq 0.25,
+            :ce 1,
+            :labor-exponents '(0.3344715806333696),
+            :industry 1,
+            :output 242164132807.1684,
+            :s 1,
+            :nature-quantities '(65829557864.94286),
+            :du 7,
+            :c 0.05,
+            :x3 53998013515.14899,
+            :product 2,
+            :labor-quantities '(53998013515.14899),
+            :production-inputs ['(3) '(1) '(1)],
+            :input-exponents '(0.35704369449641105),
+            :xe 0.05,
+            :x1 86463176651.9904,
+            :input-quantities '(86463176651.9904),
+            :x2 65829557864.94286,
+            :nature-exponents '(0.40775789400671814),
+            :a 0.25} 
+           (rc/proposal input-prices nature-prices labor-prices wc-3)))
+    (is (= {:effort 16.30645654793667,
+            :cq 10,
+            :ce 1,
+            :labor-exponents [0.2327136009634174],
+            :industry 0,
+            :x4 16.500978321509756,
+            :output 106.36021005989883,
+            :s 1,
+            :nature-quantities '(13.972169737510985),
+            :du 2,
+            :c 0.05,
+            :x3 13.972169737510985,
+            :product 2,
+            :labor-quantities '(16.500978321509756),
+            :production-inputs [[1 2] [1] [1]],
+            :input-exponents [0.18730943766373953 0.1700289432131638],
+            :xe 0.05,
+            :x1 19.922271136116837,
+            :input-quantities '(19.922271136116837 18.084314116414706),
+            :x2 18.084314116414706,
+            :nature-exponents [0.1970497669613798],
+            :a 10} 
+           (rc/proposal input-prices nature-prices labor-prices wc-4)))
+    (is (= {:effort 7.443216445365022,
+            :cq 10,
+            :ce 1,
+            :labor-exponents [0.24925680571914088],
+            :industry 0,
+            :x4 2.51998735197663,
+            :output 22.160588421020304,
+            :s 1,
+            :nature-quantities '(2.51998735197663),
+            :du 2,
+            :c 0.05,
+            :x3 2.375227452992942,
+            :product 1,
+            :labor-quantities '(3.6824516551200803),
+            :production-inputs [[1 2 3] [1] [1]],
+            :input-exponents [0.07540314861302111
+                              0.10098780164832724
+                              0.10718250832816038],
+            :xe 0.05,
+            :x1 1.6709781420621876,
+            :input-quantities '(1.6709781420621876
+                               2.237949107872213
+                               2.375227452992942),
+            :x5 3.6824516551200803,
+            :x2 2.237949107872213,
+            :nature-exponents [0.17057223193493595],
+            :a 10}
+           (rc/proposal input-prices nature-prices labor-prices wc-5)))
+    (is (= {:effort 5.720014296565836,
+            :cq 10,
+            :ce 1,
+            :labor-exponents [0.15813469104723293],
+            :industry 0,
+            :x4 0.6754391485471135,
+            :x6 1.3797173170533052,
+            :output 13.087425421167028,
+            :s 1,
+            :nature-quantities '(1.726301404508923),
+            :du 2,
+            :c 0.05,
+            :x3 1.2874866586612137,
+            :product 2,
+            :labor-quantities '(1.3797173170533052),
+            :production-inputs [[1 2 3 4] [1] [1]],
+            :input-exponents [0.07136063178539023
+                              0.09705394605246723
+                              0.09837585447317158
+                              0.0516097801370993],
+            :xe 0.05,
+            :x1 0.9339269464986589,
+            :input-quantities '(0.9339269464986589
+                               1.2701862807916346
+                               1.2874866586612137
+                               0.6754391485471135),
+            :x5 1.726301404508923,
+            :x2 1.2701862807916346,
+            :nature-exponents [0.1978580219892075],
+            :a 10}
+           (rc/proposal input-prices nature-prices labor-prices wc-6)))
+))
+
 (deftest test-run-ex001
   (let [wcs ex001/wcs
         ccs ex001/ccs
@@ -110,6 +226,30 @@
            (select-keys globals
                         [:input-prices :init-labor-price :pdlist :inputs
                          :init-intermediate-price :init-final-price])))
+    (is (= [[2518.329943276917
+             290.7735198503314
+             502.544207919467
+             15819.109593177775]
+            [808.3964206510301
+             1562.2503026323645
+             393.9958247826109
+             293.4387354891415]
+            [1000]
+            [1000]] 
+           (:supply-list iteration01)))
+    (is (= [[1216.2969702567548
+             1247.5799256408354
+             1257.5470414490735
+             1278.576062653336]
+            [4467.779622969347
+             2036.819022871612
+             1203.2857552447695
+             842.5717255704171]
+            [3300.0700581744586]
+            [3245.0589864868302]] 
+           (:demand-list iteration01)))
+    (is (= [99.5 100.49999999999999 100.49999999999999 99.5] 
+           (:final-prices iteration01)))
     (is (= ['(100
              97.93423177355632
              96.1931693902596
@@ -211,52 +351,36 @@
              0.8952007157548647
              0.4350188980580932)
             9.163866347527893]
-           (:lorenz-gini-tuple iteration01)))
-))
+           (:lorenz-gini-tuple iteration01)))))
 
-;calculate-consumer-utility
-;consume
-;assign-new-proposal
-;solution-3
-;solution-4
-;solution-5
-;solution-6
-;get-input-quantity
-;get-deltas
-;update-surpluses-prices
+(deftest consume
+  (let [cc {:effort 1, :num-workers 10, :utility-exponents [0.2379038877985112 0.23517856300054907 0.22661946853423187 0.2450284522738656], :final-demands '(0 0 0 0 0), :cy 10.037467640470766, :income 5000}
+        final-goods [1 2 3 4] 
+        final-prices [100 100 100 100]]
+    (is (= {:effort 1,
+            :num-workers 10,
+            :utility-exponents [0.2379038877985112
+                                0.23517856300054907
+                                0.22661946853423187
+                                0.2450284522738656],
+            :final-demands '(12.591099796749074
+                            12.446861563287504
+                            11.993870174233471
+                            12.96816846572995),
+            :cy 10.037467640470766,
+            :income 5000}
+           (rc/consume final-goods final-prices cc)))))
+
+
+
 ;proposal
 ;price-change
 ;other-price-change
-;get-demand-list
-;get-supply-list
 ;check-surpluses
 ;total-surplus
 ;raise-delta
 ;lower-delta
 ;rest-of-to-do
-;proceed
-
-
-;turtle 101
-;{:effort 0.5, :cq 0.25, :ce 1, :A 0.25, :labor-exponents (0.3344715806333696), :industry 1, :output 0, :du 7, :c 0.05, :product 2, :lab
-;or-quantities [0], :production-inputs [(3) (1) (1)], :input-exponents (0.35704369449641105), :xe 0.05, :S 1, :nature-exponents (0.40775
-;789400671814)}
-
-;turtle 110
-;{:effort 0.5, :cq 10, :ce 1, :A 10, :labor-exponents [0.2327136009634174], :industry 0, :output 0, :du 2, :c 0.05, :product 2, :labor-q
-;uantities [0], :production-inputs [[1 2] [1] [1]], :input-exponents [0.18730943766373953 0.1700289432131638], :xe 0.05, :S 1, :nature-e
-;xponents [0.1970497669613798]}
-
-;turtle 101
-;{:effort 0.5, :cq 10, :ce 1, :A 10, :labor-exponents [0.24925680571914088], :industry 0, :output 0, :du 2, :c 0.05, :product 1, :labor-
-;quantities [0], :production-inputs [[1 2 3] [1] [1]], :input-exponents [0.07540314861302111 0.10098780164832724 0.10718250832816038], :
-;xe 0.05, :S 1, :nature-exponents [0.17057223193493595]}
-
-;(sort (proposal [100 100 100 100] [150] [150] wc))
-
-;:input-prices	[100 100 100 100]
-;:nature-prices  [150]
-;:labor-prices   [150] 
 
 
 (deftest test-home
